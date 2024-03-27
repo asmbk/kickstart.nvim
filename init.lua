e6a2830d6209525957ec4d003b7bc01b35d7d8fc
Michal Bok <115459085+asmbk@users.noreply.github.com>
2024-03-27 23:15:35 +0100

    fix

--- init.lua
+++ init.lua
@@ -666,7 +666,7 @@ require('lazy').setup({
     },
     init = function()
       vim.o.background = 'dark'
-      vim.cmd [[colorscheme gruvbox]]
+      vim.cmd.colorscheme 'gruvbox'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
